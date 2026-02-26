13fea2f4ffa1e85e88659fbb553920ae02a3e942
brighto7700 <brighto7700@gmail.com>
2026-02-26 13:09:41 +0100

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // This generates a static 'out' folder on build
-  images: {
-    unoptimized: true, // Required for static export if using next/image
-  },
-};
-
-module.exports = nextConfig;
